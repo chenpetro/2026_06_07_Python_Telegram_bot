f496e574fdaa2df8a6714ab867ef4362c9e88ada
chenmarko77 <chenmarko77@gmail.com>
2026-06-16 23:46:20 +0300

    keyboard

--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,4 @@
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram.types import ReplyKeyboardRemove, KeyboardButton
 from commands import commands
 
@@ -13,4 +13,8 @@ def create_menu_keyboard():
     commands = ["start", "help", "settings"]  # Example commands
     for com in commands:
         builder.add(KeyboardButton(text=f"{com}"))
+    return builder.as_markup()
+
+def create_menu_keyboard():
+    builder = InlineKeyboardBuilder()
     return builder.as_markup()
